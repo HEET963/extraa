1319f8338a9a03278866eb0acf6868f7273e0a52
HEET <102133788+HEET963@users.noreply.github.com>
2022-05-30 16:52:59 +0530

    Add files via upload

---
+++ BirthDate.h
@@ -0,0 +1,31 @@
+/*==============================================
+   Name:           LiGhT's's DeAth NoTe :)
+   Student Number: 
+  ==============================================*/
+#ifndef SDDS_BIRTHDATE_H
+#define SDDS_BIRTHDATE_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+namespace sdds{
+
+   struct in{
+       char *justname;
+       int month;
+       int date;
+       int year;
+   };
+   bool openFile(const char filename[]);
+   void closeFile();
+   int noOfRecords();
+   bool read(char *stuName);
+   bool read(int &stumonth);
+   bool beginSearch(const char *filename);
+  bool readBirthDate(int month);
+    void sort();
+    void displayBirthdays();
+    void deallocate();
+    void endSearch();
+}
+#endif
